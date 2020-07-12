f8b3160f27e7e2392c16934c0cf927791d70f79e
Bas Nijholt <basnijholt@gmail.com>
2020-07-12 11:53:30 +0200

    run pre-commit

--- setup.py
+++ setup.py
@@ -1,29 +1,28 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
 
-readme = open('README.rst').read()
+readme = open("README.rst").read()
 
 setup(
-    name='text_histogram',
-    version='0.0.7',
-    description='A dependency-free library to quickly make ascii histograms from data.',
+    name="text_histogram",
+    version="1.0.0",
+    description="A dependency-free library to quickly make ascii histograms from data.",
     long_description=readme,
-    author='Andy Kish, Jehiah Czebotar',
-    author_email='agkish@gmail.com',
-    url='https://github.com/Kobold/text_histogram',
-    py_modules=['text_histogram'],
-    license='Apache License, Version 2.0',
+    author="Bas Nijholt, Andy Kish, Jehiah Czebotar",
+    author_email="agkish@gmail.com",
+    url="https://github.com/Kobold/text_histogram",
+    py_modules=["text_histogram"],
+    license="Apache License, Version 2.0",
     zip_safe=True,
     classifiers=[
         "Programming Language :: Python :: 2",
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Topic :: Terminals',
-    ]
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Topic :: Terminals",
+    ],
 )
--- text_histogram.py
+++ text_histogram.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 #
 # Copyright 2010 bit.ly
 #
@@ -16,25 +15,25 @@
 # under the License.
 
 """
-Generate a text format histogram
+Generate a text format histogram.
 
 This is a loose port to python of the Perl version at
 http://www.pandamatak.com/people/anand/xfer/histo
 
 http://github.com/bitly/data_hacks
 """
-from decimal import Decimal
 import math
-import sys
+from decimal import Decimal
 
 
-class MVSD(object):
+class MVSD:
     """ A class that calculates a running Mean / Variance / Standard Deviation"""
+
     def __init__(self):
         self.is_started = False
-        self.ss = Decimal(0) # (running) sum of square deviations from mean
-        self.m = Decimal(0) # (running) mean
-        self.total_w = Decimal(0) # weight of items seen
+        self.ss = Decimal(0)  # (running) sum of square deviations from mean
+        self.m = Decimal(0)  # (running) mean
+        self.total_w = Decimal(0)  # weight of items seen
 
     def add(self, x, w=1):
         """ add another datapoint to the Mean / Variance / Standard Deviation"""
@@ -47,11 +46,9 @@ class MVSD(object):
             self.is_started = True
         else:
             temp_w = self.total_w + w
-            self.ss += (self.total_w * w * (x - self.m) * (x - self.m )) / temp_w
+            self.ss += (self.total_w * w * (x - self.m) * (x - self.m)) / temp_w
             self.m += (x - self.m) / temp_w
             self.total_w = temp_w
-
-        # print "added %-2d mean=%0.2f var=%0.2f std=%0.2f" % (x, self.mean(), self.var(), self.sd())
 
     def var(self):
         return self.ss / self.total_w
@@ -62,32 +59,39 @@ class MVSD(object):
     def mean(self):
         return self.m
 
+
 def test_mvsd():
     mvsd = MVSD()
     for x in range(10):
         mvsd.add(x)
 
-    assert '%.2f' % mvsd.mean() == "4.50"
-    assert '%.2f' % mvsd.var() == "8.25"
-    assert '%.14f' % mvsd.sd() == "2.87228132326901"
+    assert "%.2f" % mvsd.mean() == "4.50"
+    assert "%.2f" % mvsd.var() == "8.25"
+    assert "%.14f" % mvsd.sd() == "2.87228132326901"
+
 
 def median(values):
     length = len(values)
-    if length%2:
-        median_indeces = [length/2]
+    if length % 2:
+        median_indeces = [length / 2]
     else:
-        median_indeces = [length/2-1, length/2]
+        median_indeces = [length / 2 - 1, length / 2]
 
     values = sorted(values)
     return sum([values[i] for i in median_indeces]) / len(median_indeces)
 
+
 def test_median():
-    assert 6 == median([8,7,9,1,2,6,3]) # odd-sized list
-    assert 4 == median([4,5,2,1,9,10]) # even-sized int list. (4+5)/2 = 4
-    assert "4.50" == "%.2f" % median([4.0,5,2,1,9,10]) #even-sized float list. (4.0+5)/2 = 4.5
+    assert 6 == median([8, 7, 9, 1, 2, 6, 3])  # odd-sized list
+    assert 4 == median([4, 5, 2, 1, 9, 10])  # even-sized int list. (4+5)/2 = 4
+    assert "4.50" == "%.2f" % median(
+        [4.0, 5, 2, 1, 9, 10]
+    )  # even-sized float list. (4.0+5)/2 = 4.5
 
 
-def histogram(stream, minimum=None, maximum=None, buckets=None, custbuckets=None, calc_msvd=True):
+def histogram(  # noqa: C901
+    stream, minimum=None, maximum=None, buckets=None, custbuckets=None, calc_msvd=True
+):
     """
     Loop over the stream and add each entry to the dataset, printing out at the end
 
@@ -115,14 +119,14 @@ def histogram(stream, minimum=None, maximum=None, buckets=None, custbuckets=None
         max_v = max(data)
 
     if not max_v > min_v:
-        raise ValueError('max must be > min. max:%s min:%s' % (max_v, min_v))
+        raise ValueError(f"max must be > min. max:{max_v} min:{min_v}")
     diff = max_v - min_v
 
     boundaries = []
     bucket_counts = []
 
     if custbuckets:
-        bound = custbuckets.split(',')
+        bound = custbuckets.split(",")
         bound_sort = sorted(map(Decimal, bound))
 
         # if the last value is smaller than the maximum, replace it
@@ -143,7 +147,7 @@ def histogram(stream, minimum=None, maximum=None, buckets=None, custbuckets=None
     else:
         buckets = buckets or 10
         if buckets <= 0:
-            raise ValueError('# of buckets must be > 0')
+            raise ValueError("# of buckets must be > 0")
         step = diff / buckets
         bucket_counts = [0 for x in range(buckets)]
         for x in range(buckets):
@@ -154,17 +158,17 @@ def histogram(stream, minimum=None, maximum=None, buckets=None, custbuckets=None
     mvsd = MVSD()
     accepted_data = []
     for value in data:
-        samples +=1
+        samples += 1
         if calc_msvd:
             mvsd.add(value)
             accepted_data.append(value)
         # find the bucket this goes in
         if value < min_v or value > max_v:
-            skipped +=1
+            skipped += 1
             continue
         for bucket_postion, boundary in enumerate(boundaries):
             if value <= boundary:
-                bucket_counts[bucket_postion] +=1
+                bucket_counts[bucket_postion] += 1
                 break
 
     # auto-pick the hash scale
@@ -173,9 +177,12 @@ def histogram(stream, minimum=None, maximum=None, buckets=None, custbuckets=None
 
     print("# NumSamples = %d; Min = %0.2f; Max = %0.2f" % (samples, min_v, max_v))
     if skipped:
-        print("# %d value%s outside of min/max" % (skipped, skipped > 1 and 's' or ''))
+        print("# %d value%s outside of min/max" % (skipped, skipped > 1 and "s" or ""))
     if calc_msvd:
-        print("# Mean = %f; Variance = %f; SD = %f; Median %f" % (mvsd.mean(), mvsd.var(), mvsd.sd(), median(accepted_data)))
+        print(
+            "# Mean = %f; Variance = %f; SD = %f; Median %f"
+            % (mvsd.mean(), mvsd.var(), mvsd.sd(), median(accepted_data))
+        )
     print("# each ∎ represents a count of %d" % bucket_scale)
     bucket_min = min_v
     bucket_max = min_v
@@ -186,5 +193,7 @@ def histogram(stream, minimum=None, maximum=None, buckets=None, custbuckets=None
         star_count = 0
         if bucket_count:
             star_count = bucket_count / bucket_scale
-        print('%10.4f - %10.4f [%6d]: %s' % (bucket_min, bucket_max, bucket_count, '∎' * star_count))
-
+        print(
+            "%10.4f - %10.4f [%6d]: %s"
+            % (bucket_min, bucket_max, bucket_count, "∎" * star_count)
+        )
